130fc11236cd48696081071f021655eb23f627f0
Adam Hefnider <ahefnider@yahoo.com>
2016-05-01 16:30:23 -0500

    First commit, idk if I can do pro mode but i did hard mode

---
+++ forms.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    var array = [];
+    var totalSalary = 0;
+    $('#employeeinfo').on('submit', function(event) {
+      event.preventDefault();
+
+      var values = {};
+      $.each($('#employeeinfo').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+      })
+
+      console.log(values);
+;
+
+//clear inputs
+$('#employeeinfo').find('input[type=text]').val('');
+
+
+
+   //Delete Button. Maybe. Only if it actually works.
+   $('#container').on('click', '.deleteMe', deleted);
+   function deleted() {
+  $(this).closest('.person').remove();
+ console.log("Delete");
+
+};
+
+
+
+
+      appendDom(values);
+      addSalary(values);
+    });
+
+    function appendDom(empInfo) {
+      $('#container').append('<div class="person"></div>');
+      var $el = $('#container').children().last();
+
+      $el.append('<p>First Name: ' + empInfo.employeefirstname + '</p>');
+      $el.append('<p>Last Name: ' + empInfo.employeelastname + '</p>');
+      $el.append('<p>ID Number: ' + empInfo.employeeId + '</p>');
+      $el.append('<p>Job Title: ' + empInfo.employeeJobTitle + '</p>');
+      $el.append('<p>Yearly Salary: ' + empInfo.employeeSalary + '</p>');
+      $el.append('<button type="button" class="deleteMe" name="deleteMe">Delete</button>');
+
+    }
+    function addSalary(empInfo) {
+      var empSalary = empInfo.employeeSalary;
+      totalSalary += Number(empSalary);
+      $('.salary').remove();
+      $('#allSalary').append('<p class = salary>Total Salary:' + totalSalary + '</p>')
+    }
+
+
+
+});
